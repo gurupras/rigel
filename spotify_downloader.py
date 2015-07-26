4996e2eb32b058f07fedf141ddc5ac5225319d62
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2015-07-26 13:21:50 -0400

    Added scripts to download spotify songs via youtube

---
+++ spotify_downloader.py
@@ -0,0 +1,65 @@
+import os,sys,argparse
+import json
+import shlex
+import logging
+import unicodedata
+import subprocess
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('spotify_downloader')
+
+import youtube
+from spotify import Spotify
+
+def setup_parser():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--out', default='out')
+	return parser
+
+def to_ascii(string):
+	return unicodedata.normalize('NFD', string.decode('utf-8')).encode('ascii', 'ignore')
+
+def main(argv):
+	parser = setup_parser()
+	args = parser.parse_args(argv[1:])
+
+	spotify = Spotify()
+	spotify.authenticate()
+
+	if not os.path.exists(args.out):
+		os.makedirs(args.out)
+
+	for track_list in spotify.get_user_tracks():
+		for track in track_list['items']:
+			track = track['track']
+			artist = None
+			title = None
+			try:
+				artist = to_ascii(track['artists'][0]['name'].encode('utf-8'))
+				title = to_ascii(track['name'].encode('utf-8'))
+				assert artist and title
+			except Exception, e:
+				logger.critical('FAIL/%s\n - %s', json.dumps(track, indent=4), e)
+				raise e
+				print json.dumps(track, indent=4)
+				sys.exit(-1)
+
+			name = '%s - %s.mp3' % (title, artist)
+			out = os.path.join(args.out, '%s' % (name))
+			try:
+				cmdline = 'youtube.py --artist="%s" --track="%s" --out="%s"' % (artist, title, out)
+				logger.debug('Downloading %s' % (out))
+				youtube.main(shlex.split(cmdline))
+				logger.info('OK/%s' % (name))
+			except youtube.FileExistsException, e:
+				logger.info('SKIP/%s - %s', name, e)
+			except youtube.NoSearchResultException, e:
+				logger.critical('FAIL/%s - %s', name, e)
+			except youtube.FileLengthMismatchException, e:
+				logger.warn('SKIP/%s - %s', name, e)
+			except Exception, e:
+				logger.critical('FAIL/%s - %s', name, e)
+			
+
+if __name__ == '__main__':
+	main(sys.argv)
---
+++ youtube.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python
+
+import os,sys,argparse
+import json
+import logging
+import requests,urllib2,urllib
+import time
+import unicodedata
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+
+import subprocess
+
+logger = logging.getLogger('youtube')
+
+class FileExistsException(Exception):
+	pass
+class FileLengthMismatchException(Exception):
+	pass
+class NoSearchResultException(Exception):
+	pass
+class YoutubeInMp3Exception(Exception):
+	pass
+
+# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
+# tab of
+#	 https://cloud.google.com/console
+# Please ensure that you have enabled the YouTube Data API for your project.
+DEVELOPER_KEY = "AIzaSyAH3auSKLjRE2jJbN4b6MYG4uwlIb2jGQk"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+def youtube_search(options):
+	youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+		developerKey=DEVELOPER_KEY)
+
+	# Call the search.list method to retrieve results matching the specified
+	# query term.
+	try:
+		search_response = youtube.search().list(
+			q=options.artist + ' ' + options.track,
+			part="id,snippet",
+			order='viewCount',
+			maxResults=options.max_results
+		).execute()
+	except Exception, e:
+		logger.critical('Failed to perform youtube search: %s' % (e))
+		raise e
+
+	videos = []
+
+	# Add each result to the appropriate list, and then display the lists of
+	# matching videos, channels, and playlists.
+	owner_whitelist = ['vevo', 'records', 'sony']
+	keyword_blacklist = ['full album', 'album']
+
+	for search_result in search_response.get("items", []):
+		if search_result["id"]["kind"] == "youtube#video":
+			info = search_result['snippet']
+			for x in owner_whitelist:
+				if x in info['channelTitle'].lower() and \
+						options.track.lower() in info['title'].lower():
+					videos.append(search_result)
+
+	if len(videos) == 0:
+		for search_result in search_response.get("items", []):
+			if search_result["id"]["kind"] == "youtube#video":
+				info = search_result['snippet']
+				title = info['title'].lower()
+				keywords = [options.artist, options.track]
+				failed = False
+				for word in keyword_blacklist:
+					if word.lower() in title:
+						failed = True
+#				for word in keywords:
+#					if word.lower() not in title:
+#						failed = True
+				if not failed:
+					videos.append(search_result)
+
+	for v in videos:
+		logger.debug(json.dumps(v, indent=4))	
+	return videos
+
+def create_youtube_url(id):
+	return 'http://www.youtube.com/watch?v=%s' % (id)
+
+def download(youtube_url, out):
+	base_dir = os.path.abspath(os.path.dirname(__file__))
+	youtube_dl = os.path.join(base_dir, 'youtube-dl')
+	cmd = '%s %s -x -o "%s" --audio-format "mp3"' % (youtube_dl, youtube_url, out)
+	logger.info(cmd)
+	if os.path.exists(out):
+		raise FileExistsException('File Exists!')
+	p = subprocess.Popen(cmd, shell=True)
+	p.wait()
+
+def setup_parser():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--artist", help="Search term", default="Taylor Swift")
+	parser.add_argument("--track", help="Search term", default="Blank Space")
+	parser.add_argument("--out", help="Output file", default='Blank Space - Taylor Swift.mp3')
+	parser.add_argument("--max-results", help="Max results", default=5)
+	return parser
+
+def main(argv):
+	parser = setup_parser()
+	args = parser.parse_args(argv[1:])
+
+	try:
+		results = youtube_search(args)
+		if len(results) < 1: 
+			raise NoSearchResultException("Did not find any results for query string '%s %s'" % (args.artist, args.track))
+		result = results[0]
+		url = create_youtube_url(result['id']['videoId'])
+		download(url, args.out)
+	except HttpError, e:
+		print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
+
+
+if __name__ == "__main__":
+	main(sys.argv)
